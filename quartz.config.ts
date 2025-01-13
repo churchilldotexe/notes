260bfe6f58ed8b1c6a0c1708287b81f1057fd9f9
churchilldotexe <churchillexe@gmail.com>
2025-01-13 18:17:12 +0800

    Quartz sync: Jan 13, 2025, 6:17 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "notes.tingexe.cc",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
